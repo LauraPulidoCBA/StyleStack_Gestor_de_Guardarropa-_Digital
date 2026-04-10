43984953a1bb4b0edeebf85505c1fb94d42e8282
natalia2208 <collazosk25@gmail.com>
2026-04-09 21:29:06 -0500

    feat: connect navigation to add clothing screen

--- stylestack/lib/screens/home_screen.dart
+++ stylestack/lib/screens/home_screen.dart
@@ -1,24 +1,35 @@
 import 'package:flutter/material.dart';
+import '../providers/wardrobe_provider.dart';
+import 'add_clothing_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final wardrobe = Provider.of<WardrobeProvider>(context);
 
-    
     return Scaffold(
       appBar: AppBar(title: Text("StyleStack")),
       body: ListView.builder(
-  itemCount: wardrobe.clothes.length,
-  itemBuilder: (context, index) {
-    final item = wardrobe.clothes[index];
+        itemCount: wardrobe.clothes.length,
+        itemBuilder: (context, index) {
+          final item = wardrobe.clothes[index];
 
-    return ListTile(
-      title: Text(item.name),
-      subtitle: Text(item.category),
-    );
-  },
-),
+          return ListTile(
+            title: Text(item.name),
+            subtitle: Text(item.category),
+          );
+        },
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => AddClothingScreen()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
