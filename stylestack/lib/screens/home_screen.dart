e8b3ba3f2f35de24283309b1b945b871145ef434
natalia2208 <collazosk25@gmail.com>
2026-04-09 21:25:49 -0500

    feat: display clothing list from provider

--- stylestack/lib/screens/home_screen.dart
+++ stylestack/lib/screens/home_screen.dart
@@ -3,11 +3,22 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wardrobe = Provider.of<WardrobeProvider>(context);
+
+    
     return Scaffold(
       appBar: AppBar(title: Text("StyleStack")),
-      body: Center(
-        child: Text("Bienvenida a StyleStack"),
-      ),
+      body: ListView.builder(
+  itemCount: wardrobe.clothes.length,
+  itemBuilder: (context, index) {
+    final item = wardrobe.clothes[index];
+
+    return ListTile(
+      title: Text(item.name),
+      subtitle: Text(item.category),
+    );
+  },
+),
     );
   }
 }
